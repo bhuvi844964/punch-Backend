03ff60361620b1a26907a337caaea9a8550f12cb
Bhuvnesh Sharma <104725390+bhuvi844964@users.noreply.github.com>
2023-02-27 17:43:33 +0530

    Update attendanceController.js

--- controllers/attendanceController.js
+++ controllers/attendanceController.js
@@ -15,15 +15,18 @@ module.exports.attendance = async function (req, res) {
     if (!Date || Date == "") {
       return res.status(400).send({ status: false, message: "Please provide Date" });
     }
+    
+     let existingPunchIn = await attendanceModel.findOne({ PunchIn  });
+    
+     if (!existingPunchIn) {
+      return res.status(400).send({ status: false, message: "Already Punch" });
+    }
+    
     if (!PunchIn || PunchIn == "") {
       return res.status(400).send({ status: false, message: "Please provide PunchIn" });
     }
 
-//     let existingPunchIn = await attendanceModel.findOne({ PunchIn  });
-    
-//      if (!existingPunchIn) {
-//       return res.status(400).send({ status: false, message: "Already Punch" });
-//     }
+   
     
     
     let existingData = await attendanceModel.findOne({ userId: userId, Date: Date });
