dbb622e29239c8e6823dc819faf34fb8911e6d26
bhuvnesh <bhuvi844964@gmail.com>
2023-03-07 13:09:43 +0530

    tech

--- controllers/attendanceController.js
+++ controllers/attendanceController.js
@@ -2,12 +2,10 @@ const attendanceModel = require("../models/attendanceModel");
 const userModel = require('../models/userModel');
 const mongoose = require("mongoose");
 
-
-
 module.exports.attendance = async function (req, res) {
   try {
     let data = req.body;
-    let { userId, Date, PunchIn, PunchOut, longitude, latitude } = data;
+    let { userId, Date, PunchIn, PunchOut, session, longitude, latitude } = data;
 
     if (!userId || userId == "") {
       return res.status(400).send({ status: false, message: "Please provide userId" });
@@ -22,25 +20,28 @@ module.exports.attendance = async function (req, res) {
       return res.status(400).send({ status: false, message: "Please provide PunchIn" });
     }
 
-    let existingData = await attendanceModel.findOne({ userId: userId, Date: Date });
+    let existingData = await attendanceModel.findOne({ userId: userId});
     
     if (existingData) {
-      // If there is an existing attendance data for the day, update the punch-in and punch-out values
-      if (!existingData.PunchOut) {
-        // If punch-out value is not present, update the punch-out time
+      if (existingData.PunchOut) {
+        return res
+          .status(400)
+          .send({ status: false, message: "PunchOut value already exists in database for this user " });
+      }
+      if (existingData.PunchIn) {
+        return res
+          .status(400)
+          .send({ status: false, message: "PunchIn value already exists in database for this user " });
+      }
+      else {
         existingData.PunchOut = PunchOut;
-        existingData.session.push({PunchIn: PunchIn, PunchOut: PunchOut});
+        existingData.session = time_diff(existingData.PunchIn, existingData.PunchOut);
         await existingData.save();
-        return res.status(200).send({ status: true,message: "Punch-out successful" , data: existingData });
-      } else {
-        // If punch-out value is present, create a new attendance data for the day
-        let savedData = await attendanceModel.create(data);
-        return res.status(201).send({ status: true,  message: "Punch-in successful", data: savedData });
+        return res.status(200).send({ status: true,message: "punch successful" , data: existingData });
       }
     } else {
-      // If there is no existing attendance data for the day, create a new attendance data
-      let savedData = await attendanceModel.create({userId: userId, Date: Date, PunchIn: PunchIn, session: [{PunchIn: PunchIn, PunchOut: PunchOut}]});
-      return res.status(201).send({ status: true,  message: "Punch-in successful", data: savedData });
+      let savedData = await attendanceModel.create(data);
+      return res.status(201).send({ status: true,  message: "punch successful", data: savedData });
     }
   } catch (error) {
     res.status(500).send({ status: false, error: error.message });
@@ -48,73 +49,17 @@ module.exports.attendance = async function (req, res) {
 };
 
 
+function time_diff(pIntime, pOuttime) {
+  var t1parts = pOuttime.split(":");
+  var t1cm = Number(t1parts[0]) * 60 + Number(t1parts[1]);
 
+  var t2parts = pIntime.split(":");
+  var t2cm = Number(t2parts[0]) * 60 + Number(t2parts[1]);
 
-
-
-
-
-
-
-
-// module.exports.attendance = async function (req, res) {
-//   try {
-//     let data = req.body;
-//     let { userId, Date, PunchIn, PunchOut, session, longitude, latitude } = data;
-
-//     if (!userId || userId == "") {
-//       return res.status(400).send({ status: false, message: "Please provide userId" });
-//     }
-//     if (!mongoose.isValidObjectId(userId)) {
-//       return res.status(400).send({ status: false, message: "Please enter valid userId" });
-//     }
-//     if (!Date || Date == "") {
-//       return res.status(400).send({ status: false, message: "Please provide Date" });
-//     }
-//     if (!PunchIn || PunchIn == "") {
-//       return res.status(400).send({ status: false, message: "Please provide PunchIn" });
-//     }
-
-//     let existingData = await attendanceModel.findOne({ userId: userId});
-    
-//     if (existingData) {
-//       if (existingData.PunchOut) {
-//         return res
-//           .status(400)
-//           .send({ status: false, message: "PunchOut value already exists in database for this user " });
-//       }
-//       if (existingData.PunchIn) {
-//         return res
-//           .status(400)
-//           .send({ status: false, message: "PunchIn value already exists in database for this user " });
-//       }
-//       else {
-//         existingData.PunchOut = PunchOut;
-//         existingData.session = time_diff(existingData.PunchIn, existingData.PunchOut);
-//         await existingData.save();
-//         return res.status(200).send({ status: true,message: "punch successful" , data: existingData });
-//       }
-//     } else {
-//       let savedData = await attendanceModel.create(data);
-//       return res.status(201).send({ status: true,  message: "punch successful", data: savedData });
-//     }
-//   } catch (error) {
-//     res.status(500).send({ status: false, error: error.message });
-//   }
-// };
-
-
-// function time_diff(pIntime, pOuttime) {
-//   var t1parts = pOuttime.split(":");
-//   var t1cm = Number(t1parts[0]) * 60 + Number(t1parts[1]);
-
-//   var t2parts = pIntime.split(":");
-//   var t2cm = Number(t2parts[0]) * 60 + Number(t2parts[1]);
-
-//   var hour = Math.floor((t1cm - t2cm) / 60);
-//   var min = Math.floor((t1cm - t2cm) % 60);
-//   return hour + ":" + min;
-// }
+  var hour = Math.floor((t1cm - t2cm) / 60);
+  var min = Math.floor((t1cm - t2cm) % 60);
+  return hour + ":" + min;
+}
 
 
 
@@ -200,7 +145,6 @@ function time_diff(pIntime, pOuttime) {
 //   var diff = t1cm - t2cm;
 //   return diff; // return time difference in minutes
 // }
-
 
 
 module.exports.getAttendance = async function (req, res) {
